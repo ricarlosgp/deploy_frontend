b30f41c2218ce5696615b2ace93adde4853442df
ricarlos <ricarlosgp@gmail.com>
2023-04-29 17:17:26 -0300

    ../routes/auth.routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { useAuth } from '../hooks/auth';
 
 import { AppRoutes } from './app.routes';
-import { AuthRoutes } from './auth.routes';
+import { AuthRoutes } from '../routes/auth.routes';
 
 export function Routes() {
     const { user } = useAuth();
