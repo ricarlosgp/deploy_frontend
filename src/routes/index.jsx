ae4565d19f82093d369c78bf7fcf6f337cda0874
ricarlos <ricarlosgp@gmail.com>
2023-04-29 09:50:42 -0300

    removendo espaço no index.jsx

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,7 +4,6 @@ import { useAuth } from '../hooks/auth';
 import { AppRoutes } from './app.routes';
 import { AuthRoutes } from './auth.Routes';
 
-
 export function Routes() {
     const { user } = useAuth();
     return (
